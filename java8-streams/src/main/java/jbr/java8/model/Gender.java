110b0d0c02cc196c0f4fef31193ef42022650e3a
Ranjith Sekar <java2ranjith@gmail.com>
2017-04-28 23:41:51 +0530

    Streams in Java 8

---
+++ java8-streams/src/main/java/jbr/java8/model/Gender.java
@@ -0,0 +1,13 @@
+package jbr.java8.model;
+
+/**
+ * Enum for Gender.
+ * 
+ * @author Ranjith Sekar (java2ranjith@gmail.com)
+ *
+ * @date 28-Feb-2017
+ */
+public enum Gender {
+
+  MALE, FEMALE
+}
