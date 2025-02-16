869646085e5bc00006eb603d04b82f22280057ed
Ranjith Sekar <java2ranjith@gmail.com>
2025-02-16 20:48:02 +0530

    refactor

---
+++ java8-streams/src/main/java/jbr/javastreams/model/Gender.java
@@ -0,0 +1,13 @@
+package jbr.javastreams.model;
+
+/**
+ * Enum for Gender.
+ * 
+ * @author Ranjith Sekar (java2ranjith@gmail.com)
+ *
+ * @date 28-Feb-2017
+ */
+public enum Gender {
+
+  MALE, FEMALE
+}
